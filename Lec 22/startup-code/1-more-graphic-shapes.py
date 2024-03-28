b77d6e980e4bbc87b263a54d788de2af09c4143b
KamalRanaweera <ranaweer@ualberta.ca>
2024-03-27 18:28:56 -0600

    Update 1-more-graphic-shapes.py

--- Lec 22/startup-code/1-more-graphic-shapes.py
+++ Lec 22/startup-code/1-more-graphic-shapes.py
@@ -32,10 +32,9 @@ plt.show()
 ## Drawing a Pie Chart on the Current Axes
 #  =======================================
 
-labels = ["Chrome", "Internet Explorer", "Firefox", "Edge","Safari", "Sogou Explorer","Opera","Others"]
+labels = ["Chrome", "Internet Explorer", "Firefox", "Edge","Safari", "Sogou","Opera","Others"]
 marketshare = [61.64, 11.98, 11.02, 4.23, 3.79, 1.63, 1.52, 4.19] 
 explode = (0,0,0,0,0,0,0,0) 
-#explode = (0,0.05,0,0.1,0,0.1,0.05,0.1) 
 
 plt.pie(marketshare,
         explode = explode, # fraction of the radius with which to # offset each wedge
@@ -45,7 +44,36 @@ plt.pie(marketshare,
         startangle = 45) # rotates the start of the pie chart by # angle degrees counterclockwise from the # x-axis
 
 plt.title("Web Browser Marketshare - 2018")
+
 plt.show()
+
+
+
+## Pie Chart: Highlighting Segments and Savint Figure 
+#  ==================================================
+
+labels = ["Chrome", "Internet Explorer", "Firefox", "Edge","Safari", "Sogou","Opera","Others"]
+marketshare = [61.64, 11.98, 11.02, 4.23, 3.79, 1.63, 1.52, 4.19] 
+
+# Explode two segments to highlight them
+explode = (0,0, 0.5 ,0, 0.8 ,0,0,0) 
+
+plt.pie(marketshare,
+        explode = explode, # fraction of the radius with which to # offset each wedge
+        labels = labels, 
+        autopct = "%.1f%%", # string or function used to label the # wedges with their numeric value
+        shadow = False,     # Turn off the shaddow
+        startangle = 45) # rotates the start of the pie chart by # angle degrees counterclockwise from the # x-axis
+
+plt.title("Web Browser Marketshare - 2018")
+
+# NOTE: if you want to save the figure, you should do it beofre you
+#       conclude the figure with plt.show()
+plt.savefig("browser-market.jpg")
+plt.savefig("browser-market-high-resolution.jpg", dpi=600)
+
+plt.show()
+
 
 
 
