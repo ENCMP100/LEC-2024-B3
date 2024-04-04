fb3c28f9804ad55dfc4b7f364b66711f7c2b3c60
KamalRanaweera <ranaweer@ualberta.ca>
2024-04-03 22:34:34 -0600

    Lecture 23 completed code.

---
+++ Lec 23/completed-code/1-objects-and-classes.py
@@ -0,0 +1,57 @@
+## Object and Classes
+#
+# In Object Oriented Programming (OOB), 
+# each object has its own data and a set of
+# methods that acts on that data.
+#
+# A CLASS defines the data (properties) of an
+# object and its methods.
+#
+
+
+# Class Definition
+# ================
+# A simple simulated counter
+class Counter:  
+         
+    # Resets the counter value
+    def __init__(self):
+        self._value = 0
+    
+    # Advances the counter value by 1
+    def click(self):
+        self._value = self._value + 1
+
+    # Gets the counter value
+    # @return the current counter value
+    def getValue(self):
+        return self._value
+    
+    def printClicks(self):
+        print(f"Num clicks {self._value}")
+
+
+# Using the counter class in the script
+# =====================================
+tally = Counter()
+
+
+tally2 = Counter()
+
+tally.click()
+tally.printClicks()
+tally2.printClicks()
+
+tally.click()
+tally.printClicks()
+tally2.printClicks()
+
+
+tally.click()
+tally.printClicks()
+
+tally.click()
+tally.printClicks()
+
+
+
---
+++ Lec 23/completed-code/2-more-objects-and-classes.py
@@ -0,0 +1,62 @@
+## Object and Classes
+#
+# This script shows an improved counter that was used in
+# the first script. It also shows the use of counter 
+# to track tallys of two instances.
+
+
+# Class Definition
+# ================
+# A simple simulated counter
+class Counter:
+         
+    # Constructor
+    # Constructor is autimatcalled when creating an instance of the class
+    # @param owner: a string parameter to be used as the 
+    # prefix in the print method.
+    def __init__(self, owner):
+        self._value = 0
+        self._owner = owner
+    
+    # Advances the counter value by 1
+    def click(self):
+        self._value = self._value + 1
+
+    # Prints the counter value
+    def print(self):
+        print(f"{self._owner}: {self._value}")
+
+
+# Using the counter class in the script
+# =====================================
+
+ahamad = Counter("Ahamad")
+tanya = Counter("Tanya")
+   
+ahamad.click() #Increment Ahmad's count but not Tanya's
+ahamad.print() 
+tanya.print()
+
+ahamad.click() #Increment Ahmad's count but not Tanya's
+ahamad.print()
+tanya.print()
+
+tanya.click() #Increment Tanya's count but not Ahmads's
+ahamad.print()
+tanya.print()
+
+tanya.click() #Increment Tanya's count but not Ahmads's
+ahamad.print()
+tanya.print()
+
+tanya.click() #Increment Tanya's count but not Ahmads's
+ahamad.print()
+tanya.print()
+
+tanya.click() #Increment Tanya's count but not Ahmads's
+ahamad.print()
+tanya.print()
+    
+
+
+
