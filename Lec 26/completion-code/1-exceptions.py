14fa9c24036d58063363299dff465d3a60b66618
KamalRanaweera <ranaweer@ualberta.ca>
2024-04-17 17:37:18 -0600

    Lecture 26 completion code.

---
+++ Lec 26/completion-code/1-exceptions.py
@@ -0,0 +1,56 @@
+## Exceptions and Input Output
+
+def main():
+    
+    try: 
+        filename = input('Enter GPA data file (gpa[1-4].txt): ')
+        
+        fileOpenedSuccessfully = False
+        infile = open(filename) # May raise IO error
+        fileOpenedSuccessfully = True
+        
+        print("Data file opened successfully.")
+        
+        gpa = loadGPAs(infile)
+        
+        print("GPA:", gpa)
+        
+    except IOError:
+        print("File error:")
+    except ValueError as error:
+        print("Value error: " + str(error))
+    except RuntimeError as error:
+        print("Runtime error: " + str(error))
+    except Exception:
+        print("Unknown Error")
+    finally:
+        if fileOpenedSuccessfully:
+            infile.close()
+    
+    print("Program ended.")
+
+
+def loadGPAs(file):
+    gpaValues = []
+    line = file.readline()
+    while line != "":
+        val = float(line) # May raise a ValueError if the line is not numeric
+        
+        if not 0 <= val <= 4:
+            raise RuntimeError("GPA must be between 0 and 4")
+            
+        gpaValues.append(val)
+        
+        line = file.readline()
+    
+    return gpaValues
+
+
+main()
+
+           
+        
+        
+        
+            
+
---
+++ Lec 26/completion-code/3-classes.py
@@ -0,0 +1,39 @@
+
+class Clicker:
+    _lastUsedId = 0
+    
+    def __init__(self, name):
+        self._id = Clicker._lastUsedId + 1
+        Clicker._lastUsedId = self._id
+        
+        self._clicks = 0
+        self._name = name
+        
+    def click(self):
+        self._clicks = self._clicks + 1
+        print("Clicked ", self._name)
+        
+        
+    def print(self):
+        print("Clicker", self._name)
+        print("    id:", self._id)
+        print("    clicks:", self._clicks)
+        print()
+        
+
+# Using the class
+c1 = Clicker("C1")
+c2 = Clicker("C2")
+c1._clicks = 100
+
+c1.print()
+c2.print()
+
+c1.click()
+c1.click()
+c1.print()
+c2.print()
+
+c2.click()
+c1.print()
+c2.print()
